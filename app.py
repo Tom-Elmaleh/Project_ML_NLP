766f5dadbbbec9a58393d3a9978e6053eea1f053
Tom0103 <elmalehtom@gmail.com>
2024-01-24 13:35:19 +0100

    update app + prediction_note

--- app.py
+++ app.py
@@ -7,26 +7,22 @@ from sklearn.metrics.pairwise import cosine_similarity
 import re
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
-
-# Text summarization model
-def apply_summarization(text):
-    summarizer = pipeline("summarization")
-    summary = summarizer(text, max_length=100, min_length=10, do_sample=False)[0]["summary_text"]
-    return summary
-
-# Sentiment analysis model
-def apply_sentiment_analysis(text):
-    """Function which applies a sentiment analysis"""
-    classifier = pipeline(model="distilbert-base-uncased-finetuned-sst-2-english")
-    sentiments = classifier(text)
-    return sentiments[0]['label'], sentiments[0]['score']
-
-# QA model
-def apply_qa(text,query):
-    """Function to apply a QA model"""
-    oracle = pipeline(model="deepset/roberta-base-squad2")
-    answer = oracle(question=query, context=text)['answer']
-    return answer
+  
+# Preprocessing for sentiment analysis
+def clean_sentence(val):
+    """ Cette fonction permet de supprimer les caractères spéciaux et les mots de taille inférieure à 2
+    pour la tâche de summarization"""
+    # Remplacement des valeurs \n
+    val = val.replace('\n','')
+    # Suppression des caratères spéciaux
+    regex = re.compile('([^\s\w]|_)+')
+    sentence = regex.sub('', val).lower()
+    sentence = sentence.split(" ")
+    for word in list(sentence):
+        if len(word) <= 2:
+            sentence.remove(word)
+    sentence = " ".join(sentence) 
+    return sentence
 
 # Preprocessing the text
 def preprocess(text):
@@ -37,6 +33,27 @@ def preprocess(text):
     stemmer = SnowballStemmer('english')
     tokens = [stemmer.stem(word) for word in text.split() if word not in stop_words]
     return ' '.join(tokens)
+ 
+# Text summarization model
+def apply_summarization(text):
+    summarizer = pipeline("summarization")
+    summary = summarizer(text, max_length=150, min_length=40, do_sample=False)[0]["summary_text"]
+    return summary
+
+# Sentiment analysis model
+def apply_sentiment_analysis(text):
+    """Function which applies a sentiment analysis"""
+    text = clean_sentence(text)
+    classifier = pipeline(model="distilbert-base-uncased-finetuned-sst-2-english")
+    sentiments = classifier(text)[0]
+    return sentiments['label'], sentiments['score']
+
+# QA model
+def apply_qa(text,query):
+    """Function to apply a QA model"""
+    oracle = pipeline(model="deepset/roberta-base-squad2")
+    answer = oracle(question=query, context=text)['answer']
+    return answer
 
 # Information retrieval using TF-IDF and cosine similarity
 def retrieve_information(query):
@@ -44,6 +61,8 @@ def retrieve_information(query):
     df = pd.read_csv('train_set.csv',index_col=0)
     # Combine relevant informations in one column
     df['information'] =  df['avis_en'] + df['produit'] + df["assureur"].str.lower()
+    # Application preprocessing
+    df['information'] = df['information'].apply(preprocess)
     # Use Tf_idf vectorizer for the combined_text
     vectorizer = TfidfVectorizer()
     tfidf_matrix = vectorizer.fit_transform(df['information'])
@@ -56,9 +75,16 @@ def retrieve_information(query):
     df = df.sort_values(by='similarity', ascending=False)
     return df[['assureur', 'produit','avis_en','note','similarity']].head(10)
 
+# Rating prediction model
+def apply_prediction(review):
+    """Function to apply a the rating prediction model"""
+    sentiment_pipeline = pipeline("text-classification", model="nlptown/bert-base-multilingual-uncased-sentiment")
+    result = sentiment_pipeline(review)[0]
+    return result['label'],result['score']
+    
 st.title("Machine Learning for NLP Project")
 st.subheader("For each task, follow the instructions written and then click on the button *Analyze* to run the models")
-selected_action = st.selectbox("***Select the action that you want to perform***", ["Sentiment Analysis", "Summary","Information Retrieval","QA"])
+selected_action = st.selectbox("***Select the action that you want to perform***", ["Sentiment Analysis", "Summary","Information Retrieval","QA","Rating Prediction"])
 
 if selected_action == "Summary":
     st.write("****Summarization is performed with a supervised model****")
@@ -97,3 +123,13 @@ elif selected_action == "QA":
         with st.spinner("**Please wait for the answer...**"):
             answer = apply_qa(text_input_qa,query_qa)
             st.write(f"***{answer}***")
+
+elif selected_action == "Rating Prediction":
+    st.write("****Rating Prediction is performed with a supervised model****")
+    review = st.text_area("***Enter your review***", height=5)
+    analyze_button_rp = st.button("****Analyze****", key="analyze_button_rp")
+    if review and analyze_button_rp :
+        with st.spinner("**Rating prediction of the review ongoing...**"):
+            rating_prediction, score_prediction = apply_prediction(review)
+            st.write(f"***Rating predicted : {rating_prediction}***")
+            st.write(f"***Score : {score_prediction}***")
