e84f045bf644d474300041ab6fcece3a2240e0b0
Tom0103 <elmalehtom@gmail.com>
2024-01-24 20:49:39 +0100

    update init + V3

--- app_V3.py
+++ app_V3.py
@@ -7,10 +7,13 @@ from sklearn.metrics.pairwise import cosine_similarity
 import re
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
+from transformers import AutoTokenizer
 import nltk
 nltk.download('stopwords')
+import torch 
   
 # Preprocessing for sentiment analysis
+@st.cache(allow_output_mutation=True)
 def clean_sentence(val):
     """ Cette fonction permet de supprimer les caractères spéciaux et les mots de taille inférieure à 2
     pour la tâche de summarization"""
@@ -26,7 +29,7 @@ def clean_sentence(val):
     sentence = " ".join(sentence) 
     return sentence
 
-# Preprocessing the text
+@st.cache(allow_output_mutation=True)
 def preprocess(text):
     # Remove special characters and put in lower case
     text = re.sub(r'[^a-zA-Z\s]', '', text.lower())
@@ -35,35 +38,86 @@ def preprocess(text):
     stemmer = SnowballStemmer('english')
     tokens = [stemmer.stem(word) for word in text.split() if word not in stop_words]
     return ' '.join(tokens)
- 
-# Text summarization model
+
+# Token limit
+@st.cache(allow_output_mutation=True)
+def load_tokenizer(model_name):
+    return AutoTokenizer.from_pretrained(model_name)
+
+@st.cache(allow_output_mutation=True)
+def truncate_review(review, max_length, model_name):
+    """Function to take into account the token limit of models by truncating tokens"""
+    # Load the tokenizer
+    tokenizer = load_tokenizer(model_name)
+    # Tokenize the review and truncate tokens
+    tokens = tokenizer(review, return_tensors="pt", truncation=True)
+    tokens_truncated = {key: value[:, :max_length] for key, value in tokens.items()}
+    # Convert the truncated tokens to a string
+    review_truncated = tokenizer.decode(tokens_truncated["input_ids"][0], skip_special_tokens=True)
+    return review_truncated
+
+# Summarizer
+@st.cache(allow_output_mutation=True)
+def load_summarizer():
+    return pipeline("summarization", model='sshleifer/distilbart-cnn-12-6')
+
+@st.cache(allow_output_mutation=True)
 def apply_summarization(text):
-    summarizer = pipeline("summarization")
-    summary = summarizer(text, max_length=150, min_length=40, do_sample=False)[0]["summary_text"]
+    summarizer = load_summarizer()
+    text_truncated = truncate_review(text, max_length=1024, model_name='sshleifer/distilbart-cnn-12-6')
+    summary = summarizer(text_truncated, max_length=100, min_length=20, do_sample=False)[0]["summary_text"]
     summary = summary.strip()
     return summary
 
+@st.cache(allow_output_mutation=True)
+def load_classifier():
+    return  pipeline(model="distilbert-base-uncased-finetuned-sst-2-english")
+
 # Sentiment analysis model
+@st.cache(allow_output_mutation=True)
 def apply_sentiment_analysis(text):
     """Function which applies a sentiment analysis"""
     text = clean_sentence(text)
-    classifier = pipeline(model="distilbert-base-uncased-finetuned-sst-2-english")
+    classifier = load_classifier()
+    text = truncate_review(text, max_length=128, model_name='distilbert-base-uncased-finetuned-sst-2-english')
     sentiments = classifier(text)[0]
     return sentiments['label'], sentiments['score']
 
+
 # QA model
+@st.cache(allow_output_mutation=True)
+def load_qa():
+    return  pipeline(model="deepset/roberta-base-squad2")
+
+@st.cache(allow_output_mutation=True)
 def apply_qa(text,query):
     """Function to apply a QA model"""
-    oracle = pipeline(model="deepset/roberta-base-squad2")
+    oracle = load_qa()
+    text = truncate_review(text, max_length=386, model_name='deepset/roberta-base-squad2')
+    query = truncate_review(query, max_length=64, model_name='deepset/roberta-base-squad2')
     answer = oracle(question=query, context=text)['answer']
     return answer
 
+@st.cache(allow_output_mutation=True)
+def load_text_classif():
+    return  pipeline("text-classification", model="nlptown/bert-base-multilingual-uncased-sentiment")
+
+# Rating prediction model
+@st.cache(allow_output_mutation=True)
+def apply_prediction(review):
+    """Function to apply a the rating prediction model"""
+    sentiment_pipeline = load_text_classif()
+    review = truncate_review(review, max_length=512, model_name='nlptown/bert-base-multilingual-uncased-sentiment')
+    result = sentiment_pipeline(review)[0]
+    return result['label'],result['score']
+
+@st.cache(allow_output_mutation=True)
 # Information retrieval using TF-IDF and cosine similarity
 def retrieve_information(query):
     """IR system using TF-IDF and cosine similarity to give the best results associated with the query"""
     df = pd.read_csv('train_set.csv',index_col=0)
     # Combine relevant informations in one column
-    df['information'] =  df['avis_en'] + df['produit'] + df["assureur"].str.lower()
+    df['information'] =  df['avis_en'] + df['produit'] + df["assureur"].str.lower()   
     # Application preprocessing
     df['information'] = df['information'].apply(preprocess)
     # Use Tf_idf vectorizer for the combined_text
@@ -78,12 +132,6 @@ def retrieve_information(query):
     df = df.sort_values(by='similarity', ascending=False)
     return df[['assureur', 'produit','avis_en','note','similarity']].head(10)
 
-# Rating prediction model
-def apply_prediction(review):
-    """Function to apply a the rating prediction model"""
-    sentiment_pipeline = pipeline("text-classification", model="nlptown/bert-base-multilingual-uncased-sentiment")
-    result = sentiment_pipeline(review)[0]
-    return result['label'],result['score']
     
 st.title("Machine Learning for NLP Project")
 st.subheader("For each task, follow the instructions written and then click on the button *Analyze* to run the models")
